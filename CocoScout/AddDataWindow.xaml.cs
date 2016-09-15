36dfdfba8b1d6b92eb92b150b4873f634f69cfb7
Zach <zachs@team2486.org>
2016-09-14 18:28:15 -0700

    Added files.

--- CocoScout/AddDataWindow.xaml.cs
+++ CocoScout/AddDataWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CocoScout
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddDataWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddDataWindow : Window
     {
-        public MainWindow()
+        public AddDataWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddDataWindow window2 = new AddDataWindow();
-            window2.Show();
         }
     }
 }
---
+++ CocoScout/TeamListUserControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CocoScout
+{
+    /// <summary>
+    /// Interaction logic for TeamListUserControl.xaml
+    /// </summary>
+    public partial class TeamListUserControl : UserControl
+    {
+        public TeamListUserControl()
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Test = 1, TeamNumber = 2486, MatchNumber = 10, AutoScore = 250, TeleOpScore = 250, });
+            items.Add(new User() { Test = 1, TeamNumber = 2486, MatchNumber = 10, AutoScore = 250, TeleOpScore = 250, });
+            items.Add(new User() { Test = 1, TeamNumber = 2, MatchNumber = 10, AutoScore = 250, TeleOpScore = 250, });
+            lvUsers.ItemsSource = items;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvUsers.ItemsSource);
+            PropertyGroupDescription groupDescription = new PropertyGroupDescription("Test");
+            view.GroupDescriptions.Add(groupDescription);
+        }
+
+        public class User
+        {
+            public int Test { get; set; }
+
+            public int TeamNumber { get; set; }
+
+            public int MatchNumber { get; set; }
+
+            public int TeleOpScore { get; set; }
+
+            public int AutoScore { get; set; }
+        }
+    }
+}
